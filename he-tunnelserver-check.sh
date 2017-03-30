57cbde4f3d192daafd402caf32d5812404ed8836
Nils Steinger <git@n-st.de>
2017-03-31 01:12:59 +0200

    Added list format description

--- he-tunnelserver-check.sh
+++ he-tunnelserver-check.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Obtained from the "Create new tunnel" page
+# Format:
+# - entries are separated by a single space character
+# - entries consist of:
+#   * the location (with whitespace replaced with underscores)
+#   * a colon as separator
+#   * the IPv4 of the tunnel endpoint
 tunservers="Singapore,_SG:216.218.221.42 Tokyo,_JP:74.82.46.6 Amsterdam,_NL:216.66.84.46 Berlin,_DE:216.66.86.114 Budapest,_HU:216.66.87.14 Frankfurt,_DE:216.66.80.30 London,_UK:216.66.80.26 London,_UK:216.66.88.98 Paris,_FR:216.66.84.42 Prague,_CZ:216.66.86.122 Stockholm,_SE:216.66.80.90 Warsaw,_PL:216.66.80.162 Zurich,_CH:216.66.80.98 Ashburn,_VA,_US:216.66.22.2 Chicago,_IL,_US:184.105.253.14 Dallas,_TX,_US:184.105.253.10 Denver,_CO,_US:184.105.250.46 Fremont,_CA,_US:72.52.104.74 Fremont,_CA,_US:64.62.134.130 Kansas_City,_MO,_US:216.66.77.230 Los_Angeles,_CA,_US:66.220.18.42 Miami,_FL,_US:209.51.161.58 New_York,_NY,_US:209.51.161.14 Phoenix,_AZ,_US:66.220.7.82 Seattle,_WA,_US:216.218.226.238 Toronto,_ON,_CA:216.66.38.58 Winnipeg,_MB,_CA:184.105.255.26"
 tunserver_array=($tunservers)
 tunserver_count=${#tunserver_array[@]}
